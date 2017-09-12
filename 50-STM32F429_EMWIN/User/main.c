778fc9ac0e67d92d77d3fbb61885226f5eeedcb9
Unknown <witoldmatuszek@gmail.com>
2017-09-13 00:22:59 +0200

    Authors are us

--- 50-STM32F429_EMWIN/User/main.c
+++ 50-STM32F429_EMWIN/User/main.c
@@ -1,16 +1,10 @@
 /**
- *	Keil project example for emWin
- *
- *	Before you start, select your target, on the right of the "Load" button
- *
- *	@author		Tilen Majerle
- *	@email		tilen@majerle.eu
- *	@website	http://stm32f4-discovery.com
- *	@ide		Keil uVision 5
+ *	@author		Witold Matuszek, Pawel Jacalski
+ *	@website		http://stm32f4-discovery.com
+ *	@ide			Keil uVision 5
  *	@packs		STM32F4xx Keil packs version 2.2.0 or greater required
  *	@stdperiph	STM32F4xx Standard peripheral drivers version 1.4.0 or greater required
  */
-/* Include core modules */
 #include "stm32f4xx.h"
 #include "tm_stm32f4_delay.h"
 #include "tm_stm32f4_disco.h"
